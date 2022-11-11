fd3386f19332d7284d26e06b1bf7770216b4f5c3
wooseop <crazyz2000@naver.com>
2022-11-11 14:46:46 +0900

    Use QSettings to load user information

--- example_mongodb/main.cpp
+++ example_mongodb/main.cpp
@@ -1,4 +1,5 @@
 #include <QCoreApplication>
+#include <QSettings>
 
 #include <mongocxx/client.hpp>
 #include <mongocxx/instance.hpp>
@@ -11,9 +12,13 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    QSettings settings("../../config.ini", QSettings::Format::IniFormat);
+    QString userName = settings.value("database/user_name").toString();
+    QString password = settings.value("database/password").toString();
+
     mongocxx::instance inst{};
     // set username & password for test
-    const auto uri = mongocxx::uri{"mongodb+srv://<username>:<password>@cluster0.88yln.mongodb.net/?retryWrites=true&w=majority"};
+    const auto uri = mongocxx::uri{QString("mongodb+srv://%1:%2@cluster0.88yln.mongodb.net/?retryWrites=true&w=majority").arg(userName, password).toStdString()};
     mongocxx::options::client client_options;
     auto api = mongocxx::options::server_api{mongocxx::options::server_api::version::k_version_1};
     client_options.server_api_opts (api);
